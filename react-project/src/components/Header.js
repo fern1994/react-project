8efb4f4bbf4871d4038cf8762676a45b2c30c478
fern1994 <hemhongsa.1994@gmail.com>
2018-06-15 15:25:50 +0700

    add showdow header

--- react-project/src/components/Header.js
+++ react-project/src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../duck.png'
 
 const Header = () => {
   return(
-    <div className="w-100 fixed top-0 bg-white z-5">
+    <div className="w-100 fixed top-0 bg-white z-5 shadow-4">
       <nav className="dt w-100 border-box bb b--light-gray pa3 ph5-ns">
         <a className="dtc v-mid mid-gray link dim w-25" href="/" title="Home">
           <img src={logo} className="dib w2 h2 br-100" alt="Site Name" />
